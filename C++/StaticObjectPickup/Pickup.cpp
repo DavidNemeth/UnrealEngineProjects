6c20dc174f6db6da7728671c2c724826a8ca7615
Dávid Németh <dt-dave@hotmail.com>
2016-09-18 20:42:02 +0200

    Pickup
    Object pickup for any unreal engine project. (c++)

---
+++ C++/StaticObjectPickup/Pickup.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CppPickup.h"
+#include "Pickup.h"
+
+
+// Sets default values
+APickup::APickup()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PickupRoot = CreateDefaultSubobject<USceneComponent>(TEXT("PickupRoot"));
+	RootComponent = PickupRoot;
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MyMesh"));
+	PickupMesh->AttachToComponent(PickupRoot, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+
+	PickupBox = CreateDefaultSubobject<UBoxComponent>(TEXT("PickupBox"));
+	PickupBox->SetWorldScale3D(FVector(1.0f, 1.0f, 1.0f));
+	PickupBox->bGenerateOverlapEvents = true;
+	PickupBox->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnPlayerEnterPickupBox);
+	PickupBox->AttachToComponent(PickupRoot, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+
+}
+
+// Called when the game starts or when spawned
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickup::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void APickup::OnPlayerEnterPickupBox(UPrimitiveComponent * OverLappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	//Action when player walks in the pickup box, normally you want to add the item to inventory or swap it with weapon
+	Destroy();
+}
+
